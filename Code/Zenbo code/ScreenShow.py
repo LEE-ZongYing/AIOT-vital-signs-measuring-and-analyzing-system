221af47e2781a07541226b4662f3cc314ed9ce84
LEE-ZongYing <73567295+LEE-ZongYing@users.noreply.github.com>
2021-02-19 20:02:34 +0800

    修改
    ZENBO CODE

---
+++ Code/Zenbo code/ScreenShow.py
@@ -0,0 +1,27 @@
+import pyzenbo
+import pyzenbo.modules.zenbo_command as commands
+import time
+from pyzenbo.modules.dialog_system import RobotFace
+zenbo_speakSpeed = 100
+zenbo_speakPitch = 100
+zenbo_speakLanguage = 1
+zenbo = pyzenbo.connect('192.168.43.239')
+
+def event__E5_9C_96_E5_BD_A2_E4_BB_8B_E9_9D_A21__E9_A0_81_E9_9D_A21__E9_A0_85_E7_9B_AE3_Withdraw_Card_from_CardReader():
+    zenbo.robot.set_expression(RobotFace.DEFAULT, '歡迎下次再來找Zenbo紀錄生理指標唷', {'speed':zenbo_speakSpeed, 'pitch':zenbo_speakPitch, 'languageId':zenbo_speakLanguage} , sync = True)
+
+def event__E5_9C_96_E5_BD_A2_E4_BB_8B_E9_9D_A21__E9_A0_81_E9_9D_A21__E9_A0_85_E7_9B_AE2_Check_historical_record():
+    zenbo.media.play_media('', 'IMG_20201025_1939192.jpg', sync=False)# 您需要將檔案放在裝置中指定的路徑 /sdcard/Zenbo實驗室/
+    time.sleep(int(1))
+    time.sleep(int(10))
+
+def event__E5_9C_96_E5_BD_A2_E4_BB_8B_E9_9D_A21__E9_A0_81_E9_9D_A21__E9_A0_85_E7_9B_AE1_Measure_Vital_Signs():
+    zenbo.robot.set_expression(RobotFace.DEFAULT, '已同步到其他量測裝置，請開始今天的量測', {'speed':zenbo_speakSpeed, 'pitch':zenbo_speakPitch, 'languageId':zenbo_speakLanguage} , sync = True)
+
+zenbo.robot.set_expression(RobotFace.DEFAULT)
+event__E5_9C_96_E5_BD_A2_E4_BB_8B_E9_9D_A21__E9_A0_81_E9_9D_A21__E9_A0_85_E7_9B_AE2_Check_historical_record()
+time.sleep(int(10))
+
+
+zenbo.media.stop_media()
+zenbo.release()
--- Code/Zenbo code/狀態積下的zenbo.py
+++ Code/Zenbo code/狀態積下的zenbo.py
@@ -46,17 +46,17 @@ def on_vision(*args):#Called when vision service sending result.
 def listen_callback(args):
     print('callbackS')
     slu = args.get('event_slu_query', None)
-    if slu and '量測數據'==str(slu.get('app_semantic').get('originalSentence')) :
+    if slu and '量測指標'==str(slu.get('app_semantic').get('originalSentence')) :
         print(slu)
         def job():
             sdk.robot.set_expression(RobotFace.HAPPY)
-            sdk.robot.set_expression(RobotFace.DEFAULT,'好的，那請您先插入健保卡確認身分唷', {'speed':zenbo_speakSpeed, 'pitch':zenbo_speakPitch, 'languageId':zenbo_speakLanguage} , sync = True)
+            sdk.robot.set_expression(RobotFace.DEFAULT,'好的，那請您前往量測儀器進行測量哦', {'speed':zenbo_speakSpeed, 'pitch':zenbo_speakPitch, 'languageId':zenbo_speakLanguage} , sync = True)
         t = threading.Thread(target=job)
         t.start()
         if not event_listen.isSet():
             event_listen.set()
             #此時使用者會插入健保卡、State become CardOnly from initialState
-    elif slu and '查看網頁'==str(slu.get('app_semantic').get('originalSentence')) :
+    elif slu and '查看資料'==str(slu.get('app_semantic').get('originalSentence')) :
         print(slu)
         def job():
             sdk.robot.set_expression(RobotFace.HAPPY)
@@ -72,9 +72,8 @@ def say_hello_and_ask(self):
     sdk.robot.set_expression(RobotFace.HAPPY, timeout=5)
     sdk.robot.jump_to_plan(domain, 'lanuchHelloWolrd_Plan')
     SirOrMama=['先生','女士']
-    flag=1 if self.unit=='MM' else 0#server幫我篩選過，故比較沒那些難寫
-    if flag:    
-        sdk.robot.speak_and_listen(self.MeasureValue+SirOrMama[flag]+'你好,我是 Zenbo Junior，請問您想量測生理指標或是查看歷史資料網頁呢?')
+    flag=1 if self.unit=='MM' else 0#server幫我篩選過，故比較沒那些難寫   
+    sdk.robot.speak_and_listen(self.MeasureValue+SirOrMama[flag]+'你好,我是 Zenbo Junior，請問您想量測指標或是查看歷史資料呢?')
     #需要量測身體狀況，請插入健保卡以便搜尋您的歷史資料哦
 
 
@@ -98,7 +97,7 @@ class Switcher(object):#state switcher
         self.MeasureValue=MeasureValue
         self.unit=unit
         return self.method_name
-    def number_0(self):#無限開啟While看到人就打招呼
+    def number_0(self):#初始狀態，無限開啟人臉辨識就打招呼
         result = sdk.vision.request_detect_face(enable_debug_preview=True, timeout=50)
         print(result)
         is_detect_face = event_vision.wait(timeout)
@@ -112,62 +111,88 @@ class Switcher(object):#state switcher
         say_hello_and_ask(self)#->問候後會自己聽
         is_get_listening = event_listen.wait(timeout)#timeout內有聽到不用謝謝則回覆...，
         if is_get_listening:
-            print('有人要量測數據，已請她插入健保卡')
+            print('有人要量測數據')
             event_listen.clear()
             event_vision.clear()
         elif is_get_listening==0:
-            print('不用謝謝or沒人or叫出網頁')
-            event_vision.clear()
-        CardThread=threading.Thread(target=voice)
-        CardThread.start()
-        CardThread.join()
+            print('不用謝謝or沒人or叫出網頁or沒有語音辨識到，但有卡片')
         pusher.send_string("yes")
-        print('counter = {} '.format(counter))
-    def number_9():#Card+pressure
+        # print('counter = {} '.format(counter))
+    def number_9(self):#Card+pressure
+        recommandation[self.ATN]=''
         systolicBP=(String[0:int(half)-2])
         DiastolicBP=String[int(half)-1:length-4]
         greeting[counter]='以偵測到血壓訊號，目前收縮壓為'+String[0:int(half)-2]+'mmhg、擴張壓為'+String[int(half)-1:length-4]+'mmhg'+'心跳每分鐘'+String[xx]+'下'
-        if systolicBP>140:
-            recommandation[counter]='目前收縮壓偏高'
-            if DiastolicBP >90:
-                recommandation[counter]+='及舒張壓偏高，若有任何劇烈頭痛、嘔吐、四肢無力等現象，建議前往醫院做詳細的檢查，以確保自身健康沒有問題哦' 
-        #如何知道上之手的數值(左右手)
-        #程式預測，分數高就1中2低3
-        print(greeting[counter])
-    def number_12():#Card+temperature
-        recommandation[counter]=''
-        RealNumber=float(String[0:length-2])
-        greeting[counter]='以偵測到體溫訊號，目前體溫為'+str(RealNumber)+'度,請繼續量測體重、血壓以便讓AI替您做健康分析唷'
+        SPH=True if systolicBP>140 else False
+        DPH=True if DiastolicBP>90 else False
+        if SPH==False:
+            if DPH:
+                recommandation[self.ATN]='目前舒張壓偏高喔，若有任何問題歡迎在量測一次，建議能左右手血壓各量測一次，分析結果會更為準確喔'
+            else:
+                {
+                    recommandation[self.ATN]='恭喜你還非常的健康喔，保持目前的生活作息，能使你更有活力喔。'
+                }
+        else:{
+            if DPH:
+                recommandation[self.ATN]='收縮血壓、擴張血壓數據偏高，勞煩您近期多注意自己的身體，若出現頭暈、噁心、嘔吐現象請馬上前往醫院進行檢查'
+        }
+        recommandation+='請繼續量測體溫、體重以便讓Zenbo Junior繼續替您做更詳細的健康分析哦'
+        print(recommandation[counter])
+    def number_12(self):#Card+temperature
+        recommandation[self.ATN]=''
+        greeting[self.ATN]='以偵測到體溫訊號，目前體溫為'+str(self.MeasureValue)+'度,請繼續量測體重、血壓以便讓AI替您做健康分析唷'
         flag = True if RealNumber>38.0 else False :
         if flag:
-            recommandation[counter]='您的體溫過高瞜，為了您以及Zenbo的健康請一同戴上口罩八，此外如果身體有任何不適請盡速前往醫院'
+            recommandation[self.ATN]='您的體溫過高瞜，為了您以及Zenbo的健康請一同戴上口罩八，此外如果身體有任何不適請盡速前往醫院'
         else:
             if RealNumber>37.0:
-                recommandation[counter]='體溫稍高，若有運動、跑跳接為正常現象，想再次確認體溫，歡迎過1至2分鐘後，再次回來做量測'
-        print(recommandation[counter])#CheckPoint
-    def number_10():#Card+weight
-        greeting[counter]='以偵測到體重訊號，目前體重為'+String[0:length-2]+'公斤,請繼續量測體溫、血壓，以便讓AI替您做健康分析唷'
-        #recommandation[counter]='能提供Zenbo您的身高嗎? Zenbo能依照身高、體重來建議您如何維持健康'
+                recommandation[self.ATN]='體溫稍高，若有運動、跑跳皆為正常現象，想再次確認體溫，歡迎過1至2分鐘後，再次回來做量測'
+        print(recommandation[self.ATN])#CheckPoint
+    def number_10(self):#Card+weight
+        recommandation[self.ATN]='以偵測到體重訊號，目前體重為'+self.MeasureValue+'公斤,請繼續量測體溫、血壓，以便讓AI替您做健康分析唷'
+        recommandation[self.ATN]+='能提供Zenbo您的身高嗎? Zenbo能依照身高、體重來建議您如何維持健康哦'
         #如何讓user提供身高
-        print(greeting[counter])
+        print(recommandation[self.ATN])
     #-------------------1 phase and 2 phase 分隔線
-    def number_11():#Card Weight Pressure
+    def number_11(self):#Card Weight Pressure
         #使用者至第二步，故想查看整體述職的分析結果，說出數值並請使用者測量完。
-    def number_13():# Card Temperature + Pressure
+    def ChangeState(self,NewATN)
+        if NewATN == self.ATN:
+            self.method_name='number_'+str(ATN)
+            self.method=self.getattr(self, method_name,'NO')
+            
+        recommandation[self.ATN]='以量測到....訊號，數值為:'+str(self.ATN)+'請前往量測下一生理指標。'
+    def number_13(self):# Card Temperature + Pressure
+        recommandation[self.ATN]='以量測到....訊號，數值為:'+str(self.ATN)+'請前往量測下一生理指標。'
     def number_14():# Card Temperature +Weight
-        else:
-            print('CardReadProblem -->> Measurement not ')
-        return
-
+        recommandation[self.ATN]='以量測到....訊號，數值為:'+str(self.ATN)+'請前往量測下一生理指標。'
+    def FinishState(self):
+        #秀出網頁+QRcode
+        #Zenbo : 這邊是您此次量測數值的紀錄，想查看歷史量測資料皆可以以手機掃描下方QRcode，by the way 每次AI數值分析建議、結果也會一併放置在網頁上喔。
 class number_0(object):#全0時初始狀態
     def __init__(self,obj1):
-        obj1.number_0
-
+        obj1.method
     def exit(self):
-        pass
-class CTM(State):
-    def NoticeRobot(self,obj1):
-        obj1.CTM()
+class number_8(number_0):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):
+class number_9(number_8):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):
+class number_12(object):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):
+class number_10(object):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):          
+class number_0(object):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):
     # @property
     # def s(self, )
     
@@ -175,7 +200,18 @@ class CTM(State):
 
     # def send(self, unit):
     #     pass
-
+class number_0(object):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):    
+class number_0(object):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):    
+class number_0(object):#全0時初始狀態
+    def __init__(self,obj1):
+        obj1.method
+    def exit(self):    
 
 
 def run():
@@ -189,6 +225,12 @@ def run():
     CardThread.join()
     pusher.send_string("yes")
     print('counter = {} '.format(counter))
+
+class StateManager(object):
+    def ChangeState(self,NewATN,obj)
+        if NewATN == obj.ATN:
+            self.method=
+
 
 try:
     while is_looping:
