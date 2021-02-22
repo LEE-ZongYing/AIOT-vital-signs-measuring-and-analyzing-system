735969d8ad5f6b6ca6ec8e6e7d57d9fde61e9883
LEE-ZongYing <73567295+LEE-ZongYing@users.noreply.github.com>
2021-02-22 20:19:36 +0800

    螢幕放圖片okay
    zenbo

--- Code/Zenbo code/ScreenShow.py
+++ Code/Zenbo code/ScreenShow.py
@@ -12,10 +12,9 @@ def event__E5_9C_96_E5_BD_A2_E4_BB_8B_E9_9D_A21__E9_A0_81_E9_9D_A21__E9_A0_85_E7
 
 def historical_record():
     zenbo.robot.set_expression(RobotFace.DEFAULT,'您好，我是您的健康監控小幫手Zenbo，對健康有疑問都能夠來找我喔',{'speed':zenbo_speakSpeed,'pitch':zenbo_speakPitch, 'languageId':zenbo_speakLanguage})
-    SerialDATA=zenbo.media.play_media('/storage/emulated/0/Zenbo實驗室/', 'IMG_20210210_190021.jpg', sync=True,timeout=100)# 您需要將檔案放在裝置中指定的路徑 /sdcard/Zenbo實驗室/
+    zenbo.media.play_media('', 'IMG_20201025_193919.jpg', sync=True,timeout=100)# 您需要將檔案放在裝置中指定的路徑 /sdcard/Zenbo實驗室/
     time.sleep(int(1))
     time.sleep(int(10))
-    print(SerialDATA)
 def event__E5_9C_96_E5_BD_A2_E4_BB_8B_E9_9D_A21__E9_A0_81_E9_9D_A21__E9_A0_85_E7_9B_AE1_Measure_Vital_Signs():
     zenbo.robot.set_expression(RobotFace.DEFAULT, '已同步到其他量測裝置，請開始今天的量測', {'speed':zenbo_speakSpeed, 'pitch':zenbo_speakPitch, 'languageId':zenbo_speakLanguage} , sync = True)
 
