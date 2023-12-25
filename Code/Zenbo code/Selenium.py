5a01dde9585582e4a5503ee03dcc32102b14cb41
LEE-ZongYing <73567295+LEE-ZongYing@users.noreply.github.com>
2023-12-25 16:06:29 +0800

    整理
    1.去除不必要/redundant檔案

--- Code/Zenbo code/Selenium.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-import time
-
-chromedriver=r'C:\Users\User\Desktop\Designing-implementing-and-testing-an-IoT-based-vital-signs-monitoring-system-for-elderly-health-ca\Code\Zenbo code\selenium\chromedriver.exe'
-driver=webdriver.Chrome(chromedriver)
-driver.get('http://192.168.0.164:8000/qrcode')
-time.sleep(20)
